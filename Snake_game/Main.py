7678362b7b3c82221baf112186abf2dec648095c
zonedoutvinit <88423304+zonedoutvinit@users.noreply.github.com>
2021-08-04 17:28:59 +0530

    Update Main.py

--- Snake_game/Main.py
+++ Snake_game/Main.py
@@ -1,44 +1,46 @@
 import pygame
 import random
 pygame.init()
-
-white = (255, 255, 255)
+                        #init colors for the game
+white = (255, 255, 255) 
 yellow = (255, 255, 102)
 black = (0, 0, 0)
 red = (255, 0, 0)
 green = (0, 255, 0)
 blue = (0, 102, 255)
-
+                        #init height & weight of gui
 dis_width = 600
 dis_height = 600
 
 dis = pygame.display.set_mode((dis_width, dis_height))
 pygame.display.set_caption("Snake game project")
 
-clock = pygame.time.Clock()
+clock = pygame.time.Clock() #Clock for game
 
+#snake dimensions & speed
 snake_blk = 10
 snake_speed = 15
 
+#fonts used in game
 font_style = pygame.font.SysFont("comicsansms", 25)
 score_font = pygame.font.SysFont("comicsansms", 25)
 
-
+#Score projector
 def Your_score(score):
     value = score_font.render("your score:" + str(score), True, yellow)
     dis.blit(value, [0, 0])
 
-
+#Building the snake
 def our_snake(snake_blk, snake_List):
     for x in snake_List:
         pygame.draw.rect(dis, black, [x[0], x[1], snake_blk, snake_blk])
 
-
+#Message display
 def message(msg, color):
     mesg = font_style.render(msg, True, color)
     dis.blit(mesg, [dis_width/6, dis_height/3])
 
-
+#Main loop
 def gameloop():
     gameover = False
     gamecls = False
@@ -48,8 +50,10 @@ def gameloop():
     y1_change = 0
     snake_List = []
     Lenght_of_snake = 1
-    foodx = round(random.randrange(0, dis_width - snake_blk) / 10.0) * 10.0
+                                                                            #Co-ordinates of food for snake 
+    foodx = round(random.randrange(0, dis_width - snake_blk) / 10.0) * 10.0    
     foody = round(random.randrange(0, dis_height - snake_blk) / 10.0) * 10.0
+                            #Gameplay loop
     while not gameover:
         while gamecls == True:
             dis.fill(blue)
@@ -94,8 +98,8 @@ def gameloop():
         for x in snake_List[:-1]:
             if x == snake_head:
                 gamecls = True
-        our_snake(snake_blk, snake_List)
-        Your_score(Lenght_of_snake-1)
+        our_snake(snake_blk, snake_List) 
+        Your_score(Lenght_of_snake-1)  #Score counter
         pygame.display.update()
         if x1 == foodx and y1 == foody:
             foodx = round(random.randrange(0, dis_width - snake_blk)/10.0)*10.0
