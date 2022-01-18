52ba5927761cd97d2a9df4cf3c1fb04bb9e117d7
Vinit Damodar <damodarvinit425@gmail.com>
2022-01-18 21:16:30 +0530

    first commit

---
+++ express_employee_management/Users.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+        id: 101,
+        name: "vinit",
+        email: "vinit@online.com" 
+    },
+    {
+        id: 102,
+        name: "rahul",
+        email: "rahul@online.com" 
+    },
+];
+
+module.exports = users;
